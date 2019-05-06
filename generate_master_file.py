fbfba8d05ce2d92013691feb6799a40ffc497453
Vangelis Kostalas <kostalas.v@gmail.com>
2019-05-06 17:37:38 +0300

    Working prototype

---
+++ generate_master_file.py
@@ -0,0 +1,35 @@
+indices = []
+blocks = []
+with open('Blocks.txt', 'r') as block_file:
+    for line in block_file.readlines():
+        if line.startswith('#'):
+            continue
+        l, name = line.split(';')
+        start, stop = l.split('..')
+        indices.append((int(start, 16), int(stop, 16)))
+        blocks.append(name.strip())
+
+def locate_block(code):
+    for index, [start, stop] in enumerate(indices):
+        if code > stop:
+            continue
+        else:
+            if code >= start:
+                return index
+
+with open('unicode_list.txt', 'w') as target:
+    with open('Index.txt', 'r') as names:
+        for line in names.readlines():
+            if line.startswith('#'):
+                continue
+            name, code = line.strip().split('\t')
+            try:
+                num = int(code, 16)
+            except ValueError:
+                print('could not convert ' + code)
+            index = locate_block(num)
+            if index is not None:
+                target.write(name + '\t' + code + '\t' + blocks[index] + '\n')
+            else:
+                print('Code ' + str(num) + ' not found in block, char: ' + unichr(num))
+                target.write(name + '\t' + code + '\t' + '\n')
--- main.py
+++ main.py
@@ -1,8 +1,15 @@
+import sys
+sys.path.append('/home/zen/.local/lib/python2.7/site-packages/')
+
+import os
+from fuzzywuzzy import process
+
 from ulauncher.api.client.Extension import Extension
 from ulauncher.api.client.EventListener import EventListener
 from ulauncher.api.shared.event import KeywordQueryEvent, ItemEnterEvent
 from ulauncher.api.shared.item.ExtensionResultItem import ExtensionResultItem
 from ulauncher.api.shared.action.RenderResultListAction import RenderResultListAction
+from ulauncher.api.shared.action.CopyToClipboardAction import CopyToClipboardAction
 from ulauncher.api.shared.action.HideWindowAction import HideWindowAction
 
 
@@ -15,13 +22,32 @@ class DemoExtension(Extension):
 
 class KeywordQueryEventListener(EventListener):
 
+    def __init__(self):
+        f = open(os.path.dirname(sys.argv[0]) + '/unicode_list.txt', 'r')
+        data = f.readlines()
+        self.data = {}
+        # self.names = []
+        self.items = []
+        # self.blocks = []
+        for item in data:
+            item = item.strip()
+            name, code, block = item.split('\t')
+            self.data[name + ' ' + block] = (name, block, code)
+            self.items.append(name + ' ' + block)
+            # self.names.append(name)
+            # self.blocks.append(block)
+
     def on_event(self, event, extension):
         items = []
-        for i in range(5):
-            items.append(ExtensionResultItem(icon='images/icon.png',
-                                             name='Item %s' % i,
-                                             description='Item description %s' % i,
-                                             on_enter=HideWindowAction()))
+        arg = event.get_argument()
+        if arg:
+            matches = process.extract(arg, self.items, limit=15)
+            for m in matches:
+                name, block, code = self.data[m[0]]
+                items.append(ExtensionResultItem(icon='images/bookmark.svg',
+                                                 name=name + ' - ' + unichr(int(code, 16)),
+                                                 description=block,
+                                                 on_enter=CopyToClipboardAction(unichr(int(code, 16)))))
 
         return RenderResultListAction(items)
 
